7a5693ce54d127409587735a3084edc9a10da1a1
westlove <richardEjike0172@gmail.com>
2024-12-26 10:14:47 +0100

    path remove example file

--- libs/controllers/convertController.js
+++ libs/controllers/convertController.js
@@ -1,4 +1,3 @@
-import { Parser } from "json2csv";
 import { csvService } from "../services/csvService.js";
 import { pdfService } from "../services/pdfService.js";
 import { validateJSON } from "../utils/validators.js";
@@ -7,8 +6,18 @@ class ConvertController {
   async convertToCSV(req, res) {
     try {
       const jsonData = req.body;
-      const csvData = await this.convertJSONToCSV(jsonData);
+
+      if (!validateJSON(jsonData)) {
+        return res.status(400).json({
+          status: "error",
+          message: "Invalid JSON input",
+        });
+      }
+
+      const csvData = await csvService.convert(jsonData);
+
       res.setHeader("Content-Type", "text/csv");
+      res.setHeader("Content-Disposition", "attachment; filename=data.csv");
       res.status(200).send(csvData);
     } catch (error) {
       res.status(500).json({ status: "error", message: error.message });
@@ -17,7 +26,8 @@ class ConvertController {
 
   async convertToPDF(req, res) {
     try {
-      const jsonData = res.body;
+      const jsonData = req.body;
+
       if (!validateJSON(jsonData)) {
         return res.status(400).json({
           status: "error",
@@ -28,7 +38,7 @@ class ConvertController {
       const pdfData = await pdfService.convert(jsonData);
       res.setHeader("Content-Type", "application/pdf");
       res.setHeader("Content-Disposition", "attachment; filename=data.pdf");
-      res.send(pdfData);
+      res.status(200).send(pdfData);
     } catch (error) {
       res.status(500).json({
         status: "error",
@@ -36,12 +46,6 @@ class ConvertController {
       });
     }
   }
-
-  async convertJSONToCSV(jsonData) {
-    if (!validateJSON(jsonData)) {
-      throw new Error("Invalid JSON input");
-    }
-    return await csvService.convert(jsonData);
-  }
 }
+
 export const convertController = new ConvertController();
